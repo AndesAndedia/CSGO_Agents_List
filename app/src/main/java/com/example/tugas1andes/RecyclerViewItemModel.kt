bcd98ce966a092ebc901078093ae77e01f5392e7
MatahariP <adip2003@gmail.com>
2023-06-22 15:01:14 +0700

    API Configuration

--- app/src/main/java/com/example/tugas1andes/RecyclerViewItemModel.kt
+++ app/src/main/java/com/example/tugas1andes/RecyclerViewItemModel.kt
@@ -1,8 +1,18 @@
 package com.example.tugas1andes
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class RecyclerViewItemModel(
+    @field : SerializedName("gambar")
     val gambar: Int,
+    @field : SerializedName("judul")
     val judul: String,
+    @field : SerializedName("deskripsi")
     val deskripsi: String,
+    @field : SerializedName("tahun")
     val tahun: String
-)
+) : Parcelable
+
