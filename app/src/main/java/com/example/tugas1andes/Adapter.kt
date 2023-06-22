998782cd2a52a0ef06066be176bd579bc6cbc44f
AndesAndedia <100776746+AndesAndedia@users.noreply.github.com>
2023-06-22 22:42:06 +0700

    Fixing data class

--- app/src/main/java/com/example/tugas1andes/Adapter.kt
+++ app/src/main/java/com/example/tugas1andes/Adapter.kt
@@ -1,38 +1,31 @@
-package com.example.tugas1andes
-
-import android.icu.text.LocaleDisplayNames.UiListItem
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
 import com.example.tugas1andes.databinding.RecycleViewBinding
+import com.example.tugas1andes.listAgent
 
-class Adapter(private val mutableList: MutableList<RecyclerViewItemModel>): RecyclerView.Adapter<Adapter.ViewHolderCustom>() {
-    private var listData = mutableListOf<RecyclerViewItemModel>()
+class Adapter(private val mutableList: MutableList<listAgent>) : RecyclerView.Adapter<Adapter.ViewHolderCustom>() {
+    private var listData = mutableListOf<listAgent>()
 
-    inner class ViewHolderCustom(private val binding: RecycleViewBinding)
-        : RecyclerView.ViewHolder(binding.root){
-        fun bindData(item: RecyclerViewItemModel){
-            with (binding){
-                Glide.with(itemView.context).load("${item.image}").into(gambarItem)
+    inner class ViewHolderCustom(private val binding: RecycleViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bindData(item: listAgent) {
+            with(binding) {
+                Glide.with(itemView.context).load(item.image).into(gambarItem)
                 judulItem.text = item.name
                 deskripsiItem.text = item.description
                 rarityItem.text = item.rarity
             }
-            // Haloo
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.ViewHolderCustom {
-        return ViewHolderCustom(
-            RecycleViewBinding.inflate(
-                LayoutInflater.from(parent.context), parent, false
-            )
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderCustom {
+        val binding = RecycleViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolderCustom(binding)
     }
 
-    override fun onBindViewHolder(holder: Adapter.ViewHolderCustom, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolderCustom, position: Int) {
         holder.bindData(listData[position])
     }
 
@@ -40,9 +33,8 @@ class Adapter(private val mutableList: MutableList<RecyclerViewItemModel>): Recy
         return listData.size
     }
 
-    fun isiList(listItem: MutableList<RecyclerViewItemModel>){
+    fun isiList(listItem: MutableList<listAgent>) {
         listData.clear()
         listData.addAll(listItem)
     }
-
 }
